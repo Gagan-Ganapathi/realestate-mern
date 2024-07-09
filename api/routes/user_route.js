648db6f69df35ea1b411201ba4a4ae1f803c0092
Gagan-Ganapathi <gagan.g196016@gmail.com>
2024-07-10 01:34:10 +0530

    create signup api route

--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {test} from "../controllers/user_controller.js";
+import { test } from "../controllers/user_controller.js";
+
 
 const router =express.Router();
 
