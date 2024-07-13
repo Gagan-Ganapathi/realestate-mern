30feb6664af2eaf81dc28d9ac027ba7bcf9e1a22
Gagan-Ganapathi <gagan.g196016@gmail.com>
2024-07-14 00:38:36 +0530

    add create listing api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserFailure,
+   signOutUserSuccess, 
+   signOutUserStart
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -108,7 +111,24 @@ export default function Profile() {
     }
   };
 
-  
+  const handleSignOut = async () => {
+
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+  };
+
+
+
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -172,7 +192,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
