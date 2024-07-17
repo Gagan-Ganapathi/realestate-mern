dfca62c92e2ded01dd5011ac7fd0f1ab91fec384
Gagan-Ganapathi <gagan.g196016@gmail.com>
2024-07-18 01:42:35 +0530

    add pre installation

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -27,7 +27,7 @@ export default function ListingItem(props) {
             {props.listing.description}
           </p>
           <p className='text-slate-500 mt-2 font-semibold '>
-            $
+          ₹
             {props.listing.offer
               ? props.listing.discountPrice.toLocaleString('en-US')
               : props.listing.regularPrice.toLocaleString('en-US')}
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,15 @@
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About  Estate Ease</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Estate Ease, your premier destination for all your real estate needs. Our innovative app provides a seamless experience for users to explore and list properties with ease. Whether you are looking to find your dream home, rent a cozy apartment, or sell your property, Estate Ease is here to make the process effortless and efficient.
+
+</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>At Estate Ease, we are committed to making your real estate journey as smooth and hassle-free as possible. Join us today and experience the future of real estate. Whether you are searching for your next home or looking to list your property, Estate Ease is your ultimate real estate companion..</p>
     </div>
   )
 }
